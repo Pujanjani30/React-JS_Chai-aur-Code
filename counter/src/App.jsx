b89ebf86e99421e30df6c6e1fd6aa1ee0f992fc4
Pujan <pujan.jani@saeculumsolutions.com>
2023-09-08 17:52:58 +0530

    commit

--- counter/src/App.jsx
+++ counter/src/App.jsx
@@ -8,7 +8,20 @@ function App() {
   const addValue = () =>{
     // counter = counter + 1;
     if(counter < 20){
-      setCounter(counter + 1);
+
+      // setCounter(counter + 1);
+      // setCounter(counter + 1);
+      // setCounter(counter + 1);
+      // setCounter(counter + 1);
+
+      // here we are using setCounter 4 times but it will only update the value once
+      // because react will only update the value once in a single render cycle
+      // so we can use the previous value of counter to update the value
+      
+      setCounter(prevValue => prevValue + 1);
+      setCounter(prevValue => prevValue + 1);
+      setCounter(prevValue => prevValue + 1);
+      setCounter(prevValue => prevValue + 1);
     }
     // console.log('Clicked', counter);
   }
